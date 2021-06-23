7718bf49416d8aaa6694c2605173d792787de167
jmscreation <jmscreator@gmail.com>
2021-06-22 22:07:51 -0400

    Updated json parse wrapper
    Arithmetic type-free json parsing

--- src/completed/jsonloader.h
+++ src/completed/jsonloader.h
@@ -4,6 +4,7 @@
 //include rapidjson Parse Library
 #include <string>
 #include <iostream>
+#include <type_traits>
 
 #define RAPIDJSON_HAS_STDSTRING 1
 #include "rapidjson/document.h"
@@ -25,6 +26,13 @@ namespace json {
 
     template<class T>
     bool loadProperty(const Object& config, const char* name, T& parameter) {
+        if(std::is_pointer<T>::value){
+            if(displayErrors){
+                std::cout << "Pointer cannot be loaded from JSON\n";
+            }
+            return false;
+        }
+
         if(!_ready){
             if(displayErrors){
                 std::cout << "Must parse JSON data before loading property\n";
@@ -33,6 +41,17 @@ namespace json {
         }
 
         if(config.IsObject() && config.HasMember(name)){
+            if(std::is_arithmetic<T>::value && config[name].IsNumber()) {
+                if(config[name].IsUint()) { parameter = config[name].GetUint(); return true; }
+                if(config[name].IsInt()) { parameter = config[name].GetInt(); return true; }
+
+                if(config[name].IsInt64()) { parameter = config[name].GetInt64(); return true; }
+                if(config[name].IsUint64()) { parameter = config[name].GetUint64(); return true; }
+
+                if(config[name].IsDouble()) { parameter = config[name].GetDouble(); return true; }
+                if(config[name].IsFloat()) { parameter = config[name].GetFloat(); return true; }
+            }
+
             if(config[name].Is<T>()){
                 parameter = config[name].Get<T>();
                 return true;
