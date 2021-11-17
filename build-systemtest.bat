976e7393d3a406735bdb28f38df7ab57d25f320b
jmscreation <jmscreator@gmail.com>
2021-11-16 23:27:37 -0500

    Library name patches
    patched library names

--- build-systemtest.bat
+++ build-systemtest.bat
@@ -25,7 +25,7 @@ set ASYNC_BUILD=1
 :: Configure Source For Compiling And Additional Custom Library Directories / Names
 set SOURCE_DIRECTORIES=systemtest src srclibs resources
 set INCLUDE_DIRECTORIES=systemtest src srclibs resources
-set LIBRARY_DIRECTORIES=libraries\librapidjson libraries\libjsonloader libraries\libportaudio libraries\libaudio libraries\libfreetype libraries\libpng libraries\libz
+set LIBRARY_DIRECTORIES=libraries\librapidjson-main libraries\libjsonloader-main libraries\libportaudio-main libraries\libaudio-main libraries\libfreetype-main libraries\libpng-main libraries\libz-main
 set LIBRARY_NAMES=png64 zlib64 freetype64 portaudio64 setupapi winmm user32 gdi32 opengl32 Shlwapi dwmapi stdc++fs
 
 :: Custom Library Support Directory Names
