df5a1e98bfb701e4032a4a8ab7ceff239e27fa56
jmscreation <36322006+jmscreation@users.noreply.github.com>
2022-03-18 16:45:55 -0400

    Update datamanip.cpp
    fixed missing namespace

--- src/datamanip.cpp
+++ src/datamanip.cpp
@@ -1,5 +1,6 @@
 #include "datamanip.h"
-
-DataManipulator::DataManipulator(): readonly(false), error(false), pos(0), length(0), wdata( std::make_unique<std::stringstream>() ) {}
-DataManipulator::DataManipulator(const char* data, size_t length):
-                                    readonly(true), error(false), pos(0), length(length), rdata(data) {}
+namespace gapi {
+    DataManipulator::DataManipulator(): readonly(false), error(false), pos(0), length(0), wdata( std::make_unique<std::stringstream>() ) {}
+    DataManipulator::DataManipulator(const char* data, size_t length):
+                                        readonly(true), error(false), pos(0), length(length), rdata(data) {}
+}
